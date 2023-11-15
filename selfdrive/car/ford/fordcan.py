56a52b4d6bd0bf813443648f056b597071644149
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2023-11-15 12:02:58 -0500

    Update fordcan.py

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -130,9 +130,9 @@ def create_acc_msg(packer, CAN: CanBus, long_active: bool, gas: float, accel: fl
 
   decel = accel < 0 and long_active
   values = {
-    "AccBrkTot_A_Rq": accel,                          # Brake total accel request: [-20|11.9449] m/s^2
+    "AccBrkTot_A_Rq": gas,                            # Brake total accel request: [-20|11.9449] m/s^2
     "Cmbb_B_Enbl": 1 if long_active else 0,           # Enabled: 0=No, 1=Yes
-    "AccPrpl_A_Rq": -5,                 # Acceleration request: [-5|5.23] m/s^2
+    "AccPrpl_A_Rq": accel,                            # Acceleration request: [-5|5.23] m/s^2
     "AccResumEnbl_B_Rq": 1 if long_active else 0,
     # TODO: we may be able to improve braking response by utilizing pre-charging better
     "AccBrkPrchg_B_Rq": 0,             # Pre-charge brake request: 0=No, 1=Yes
